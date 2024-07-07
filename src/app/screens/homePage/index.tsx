ae6c2004f037d53545667238092444a48f8ff73b
mumar9504 <minu0709ms@gmail.com>
2024-07-07 21:33:04 +0900

    fix: develop burak screen based components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from "@mui/material";
 
-export function About() {
-  return <Container>About</Container>;
+export function HomePage() {
+  return <Container>HomePage</Container>;
 }
--- src/app/screens/userPage/index.tsx
+++ src/app/screens/userPage/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from "@mui/material";
 
-export function Users() {
-  return <Container>Users</Container>;
+export function UserPage() {
+  return <Container>UserPage</Container>;
 }
