8a9e4cf2f0e1c50a40738ba2cc68702ed2df8d3c
mumar9504 <minu0709ms@gmail.com>
2024-07-07 19:35:17 +0900

    feat: MUI customized integration

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,25 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
 import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        
+        <App />
+        
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
