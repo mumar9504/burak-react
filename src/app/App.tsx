6aa6e7ac3dcfad7d1cc6238c1278a0d7ae7d04b4
mumar9504 <minu0709ms@gmail.com>
2024-07-09 18:09:41 +0900

    fix: organize header and footer components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,49 +1,38 @@
 import React from "react";
 import "../css/app.css";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { Link, Route, Switch } from "react-router-dom";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage/index";
-import { ProductsPage } from './screens/productsPage/index';
-import { OrdersPage } from './screens/ordersPage/index';
-import { UserPage } from './screens/userPage/index';
+import { ProductsPage } from "./screens/productsPage/index";
+import { OrdersPage } from "./screens/ordersPage/index";
+import { UserPage } from "./screens/userPage/index";
+import { HomeNavbar } from "./components/headers/HomeNavbar";
+import { OtherNavbar } from "./components/headers/OtherNavbar";
+import { Footer } from "./components/footer";
 
 function App() {
-  return (
-    <div>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">HomePage</Link>
-        </li>
-        <li>
-          <Link to="/products">ProductsPage</Link>
-        </li>
-        <li>
-          <Link to="/orders">OrdersPage</Link>
-        </li>
-        <li>
-          <Link to="/member-page">UserPage</Link>
-        </li>
-      </ul>
-    </nav>
-    
-    <Switch> 
-      <Route path="/products">
-        <ProductsPage />
-      </Route>
-      <Route path="/orders">
-        <OrdersPage />
-      </Route>
-      <Route path="/member-page">
-        <UserPage />
-      </Route>
-      <Route path="/">
-        <HomePage />
-      </Route>
-    </Switch>
-  </div>
-  );
+  const location = useLocation();
 
+  return (
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
