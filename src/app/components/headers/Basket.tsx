cb50ba476c95e9d8840000ce88385c538d4af7f1
mumar9504 <minu0709ms@gmail.com>
2024-08-09 05:04:55 +0900

    feat: develop createOrder and retrieve pausedOrders data

--- src/app/components/headers/Basket.tsx
+++ src/app/components/headers/Basket.tsx
@@ -8,7 +8,10 @@ import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useHistory } from "react-router-dom";
 import { CartItem } from "../../../lib/types/search";
-import { serverApi } from "../../../lib/config";
+import { Messages, serverApi } from "../../../lib/config";
+import { sweetErrorHandling } from "../../../lib/sweetAlert";
+import { useGlobals } from "../../hooks/useGlobals";
+import OrderService from "../../services/OrderService";
 
 interface BasketProps {
   cartItems: CartItem[];
@@ -20,7 +23,7 @@ interface BasketProps {
 
 export default function Basket(props: BasketProps) {
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = props;
-  const authMember = null;
+  const { authMember } = useGlobals();
   const history = useHistory();
   const itemsPrice = cartItems.reduce(
     (a: number, c: CartItem) => a + c.price * c.quantity,
@@ -38,6 +41,24 @@ export default function Basket(props: BasketProps) {
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const proceedOrderHandler = async () => {
+    try {
+      handleClose();
+      if (!authMember) throw new Error(Messages.error2);
+
+      const order = new OrderService();
+      await order.createOrder(cartItems);
+
+      onDeleteAll();
+
+      // REFRESH VIA CONTEXT
+      history.push("/orders");
+    } catch (err) {
+      console.log(err);
+      sweetErrorHandling(err).then();
+    }
   };
 
   return (
@@ -141,14 +162,18 @@ export default function Basket(props: BasketProps) {
             </Box>
           </Box>
           {cartItems.length !== 0 ? (
-          <Box className={"basket-order"}>
-            <span className={"price"}>
-              Total: ${totalPrice} ({itemsPrice} + {shippingCost})
-            </span>
-            <Button startIcon={<ShoppingCartIcon />} variant={"contained"}>
-              Order
-            </Button>
-          </Box>
+            <Box className={"basket-order"}>
+              <span className={"price"}>
+                Total: ${totalPrice} ({itemsPrice} + {shippingCost})
+              </span>
+              <Button
+                onClick={proceedOrderHandler}
+                startIcon={<ShoppingCartIcon />}
+                variant={"contained"}
+              >
+                Order
+              </Button>
+            </Box>
           ) : (
             ""
           )}
--- src/app/services/OrderService.ts
+++ src/app/services/OrderService.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { serverApi } from "../../lib/config";
+import { CartItem } from "../../lib/types/search";
+import { Order, OrderInquiry, OrderItemInput } from "../../lib/types/order";
 
 class OrderService {
   private readonly path: string;
@@ -8,31 +10,44 @@ class OrderService {
     this.path = serverApi;
   }
 
-  // public async getOrders(): Promise<any> {
-  //   try {
-  //     const url = this.path + "/order";
-  //     const result = await axios.get(url);
-  //     console.log("getOrders:", result);
+  public async createOrder(input: CartItem[]): Promise<Order> {
+    try {
+      const orderItems: OrderItemInput[] = input.map((cartItem: CartItem) => {
+        return {
+          itemQuantity: cartItem.quantity,
+          itemPrice: cartItem.price,
+          productId: cartItem._id,
+        };
+      });
 
-  //     return result.data;
-  //   } catch (err) {
-  //     console.log("Error, getOrders:", err);
-  //     throw err;
-  //   }
-  // }
+      const url = this.path + "/order/create";
+      const result = await axios.post(url, orderItems, {
+        withCredentials: true,
+      });
+      console.log("createOrder:", result);
 
-  // public async createOrder(input: any): Promise<any> {
-  //   try {
-  //     const url = this.path + "/order";
-  //     const result = await axios.post(url, input, { withCredentials: true });
-  //     console.log("createOrder:", result);
+      return result.data;
+    } catch (err) {
+      console.log("Error, createOrder:", err);
+      throw err;
+    }
+  }
 
-  //     return result.data;
-  //   } catch (err) {
-  //     console.log("Error, createOrder:", err);
-  //     throw err;
-  //   }
-  // }
+  public async getMyOrders(input: OrderInquiry): Promise<Order[]> {
+    try {
+      // axios.defaults.withCredentials = true;
+      const url = `${this.path}/order/all`;
+      const query = `?page=${input.page}&limit=${input.limit}&orderStatus=${input.orderStatus}`;
+      
+      const result = await axios.get(url + query, { withCredentials: true });
+      console.log("getMyOrders:", result);
+
+      return result.data;
+    } catch (err) {
+      console.log("Error, getMyOrders:", err);
+      throw err;
+    }
+  }
 
   // public async updateOrder(input: any): Promise<any> {
   //   try {
